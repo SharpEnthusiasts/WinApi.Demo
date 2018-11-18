1e5c262f1d9e77364ec45a5f142d05ba9b236182
Adam Błaszczyk <adamix224@gmail.com>
2018-11-18 20:43:09 +0100

    Added Commands

--- src/WinAPIDemo.ClientHTTP/ViewModel/ViewModel.cs
+++ src/WinAPIDemo.ClientHTTP/ViewModel/ViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using WinAPIDemo.ClientHTTP.Model;
+using WinAPIDemo.Models;
+using static WinAPIDemo.ClientHTTP.Helpers.HttpRequestHelpers;
 
 namespace WinAPIDemo.ClientHTTP.ViewModel
 {
@@ -12,16 +16,143 @@ namespace WinAPIDemo.ClientHTTP.ViewModel
     {
         HttpRequest model = new HttpRequest();
 
+        #region ModelBinding
+
+        public string Output
+        {
+            get { return model.Output; }
+            set
+            {
+                model.Output = value;
+                OnPropertyChanged(nameof(Output));
+            }
+        }
+
         public string ServerName
         {
-            get
-            {
-                return model.ServerName;
-            }
+            get { return model.ServerName; }
             set
             {
                 model.ServerName = value;
                 OnPropertyChanged(nameof(ServerName));
+            }
+        }
+
+        public string ServerPort
+        {
+            get { return model.ServerPort; }
+            set
+            {
+                model.ServerPort = value;
+                OnPropertyChanged(nameof(ServerPort));
+            }
+        }
+
+        public string ServerEndpoint
+        {
+            get { return model.ServerEndpoint; }
+            set
+            {
+                model.ServerEndpoint = value;
+                OnPropertyChanged(nameof(ServerEndpoint));
+            }
+        }
+
+        public string Data
+        {
+            get { return model.Data; }
+            set
+            {
+                model.Data = value;
+                OnPropertyChanged(nameof(Data));
+            }
+        }
+
+        public string Headers
+        {
+            get { return model.Headers; }
+            set
+            {
+                model.Headers = value;
+                OnPropertyChanged(nameof(Headers));
+            }
+        }
+
+        public string RequestMethod
+        {
+            get { return model.RequestMethod; }
+            set
+            {
+                model.RequestMethod = value;
+                OnPropertyChanged(nameof(RequestMethod));
+            }
+        }
+
+        public IReadOnlyList<string> RequestMethods
+        {
+            get { return model.RequestMethods; }
+        }      
+
+        #endregion
+
+        private ICommand executeRequest;
+        private ICommand getAllRequest;
+
+        public ICommand ExecuteRequest
+        {
+            get
+            {
+                if (executeRequest == null)
+                    executeRequest = new RelayCommand(
+                        (object parameter) =>
+                        {
+                            var item = new ToDoItem
+                            {
+                                Title = "Clean up house",
+                                IsCompleted = false
+                            };
+                            var json = JsonConvert.SerializeObject(item);
+
+                            if(Data.Equals("Sample"))
+                            {
+                                Data = json;
+                            }
+
+                            var port = short.Parse(ServerPort);
+                            Output = HttpSendRequestHelper(ServerName, port,
+                                RequestMethod, ServerEndpoint, Headers, Data);
+                            OnPropertyChanged(nameof(Output));
+                        },
+                        (object parameter) =>
+                        {
+                            if(short.TryParse(ServerPort, out var port) == false)
+                            {
+                                Output = "INVALID PORT";
+                                OnPropertyChanged(nameof(Output));
+                                return false;
+                            }
+                            return true;
+                        });
+                return executeRequest;
+            }
+        }
+
+        public ICommand GetAllRequest
+        {
+            get
+            {
+                if (getAllRequest == null)
+                    getAllRequest = new RelayCommand(
+                        (object parameter) =>
+                        {
+                            Output = HttpSendRequestHelper("https://localhost:5001/api/todo");
+                            OnPropertyChanged(nameof(Output));
+                        },
+                        (object parameter) =>
+                        {
+                            return true;
+                        });
+                return getAllRequest;
             }
         }
 
