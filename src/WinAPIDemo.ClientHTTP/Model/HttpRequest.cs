222228311cbe78e2eb77b231855e675d7a59fd86
Adam Błaszczyk <adamix224@gmail.com>
2018-11-18 14:42:10 +0100

    Added Model and ViewModel

---
+++ src/WinAPIDemo.ClientHTTP/Model/HttpRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAPIDemo.ClientHTTP.Model
+{
+    public class HttpRequest
+    {
+        public string ServerName { get; set; }
+    }
+}
---
+++ src/WinAPIDemo.ClientHTTP/ViewModel/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinAPIDemo.ClientHTTP.Model;
+
+namespace WinAPIDemo.ClientHTTP.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        HttpRequest model = new HttpRequest();
+
+        public string ServerName
+        {
+            get
+            {
+                return model.ServerName;
+            }
+            set
+            {
+                model.ServerName = value;
+                OnPropertyChanged(nameof(ServerName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
